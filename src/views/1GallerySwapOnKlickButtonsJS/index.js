589e26806abee0cb2054e0d281d147a0403e6a14
MushroomGRIB <pugnator.artifex@gmail.com>
2022-10-16 03:02:15 +0300

    addPage4AndGeneralDebug

--- src/views/1GallerySwapOnKlickButtonsJS/index.js
+++ src/views/1GallerySwapOnKlickButtonsJS/index.js
@@ -6,30 +6,25 @@ let arr = ["EntolomaHochstetteri1", "EntolomaHochstetteri2", "EntolomaHochstette
     "EntolomaHochstetteri4", "EntolomaHochstetteri5", "EntolomaHochstetteri6",
     "EntolomaHochstetteri7",];
 
-function go_to_right() {
-
+function go_to_right(e) {
     theNum++;
-
     if (theNum == arr.length) {
         theNum = "0";
     }
-
     change_image.src = 'assets/' + arr[theNum] + '.jpg';
-
     OpenImage.href = 'assets/' + arr[theNum] + '.jpg';
+    e.preventDefault();
 }
 
-function go_to_left() {
-
+function go_to_left(e) {
     theNum--;
-
     if (theNum == "-1") {
         theNum = arr.length - 1;
     }
     console.log(theNum);
-
     change_image.src = 'assets/' + arr[theNum] + '.jpg';
-
     OpenImage.href = 'assets/' + arr[theNum] + '.jpg';
+    e.preventDefault();
+
 
 }
